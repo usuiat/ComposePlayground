6f00f85584beaf429b4c4e646f8d398cc71feeb5
usuiat <stripewani@gmail.com>
2024-11-30 23:52:16 +0900

    Fix error

--- app/src/main/java/net/engawapg/app/trial/composeplayground/MainActivity.kt
+++ app/src/main/java/net/engawapg/app/trial/composeplayground/MainActivity.kt
@@ -3,6 +3,7 @@ package net.engawapg.app.trial.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeDrawingPadding
