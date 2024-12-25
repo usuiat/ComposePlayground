230aafe48c408078e4038a3e0efa3c0820558bb8
usuiat <stripewani@gmail.com>
2024-12-25 10:49:58 +0900

    Zoomableのテスト

--- app/src/main/java/net/engawapg/app/trial/composeplayground/MainActivity.kt
+++ app/src/main/java/net/engawapg/app/trial/composeplayground/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import net.engawapg.app.trial.composeplayground.ui.theme.ComposePlaygroundTheme
+import net.engawapg.lib.zoomable.rememberZoomState
+import net.engawapg.lib.zoomable.zoomable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,5 +31,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Sample() {
-    // TODO
+    val painter = painterResource(R.drawable.fuji)
+    val zoomState = rememberZoomState(contentSize = painter.intrinsicSize)
+    Image(
+        painter = painter,
+        contentDescription = null,
+        modifier = Modifier
+            .fillMaxSize()
+            .zoomable(zoomState)
+    )
 }
