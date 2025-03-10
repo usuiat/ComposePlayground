c80da90f76b283107e4628df95b294f17d26b0b4
usuiat <atsushi-usui@cybozu.co.jp>
2025-03-10 18:37:34 +0900

    BoxScopeでalignを使った時の動作確認

--- app/src/main/java/net/engawapg/app/trial/composeplayground/MainActivity.kt
+++ app/src/main/java/net/engawapg/app/trial/composeplayground/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import net.engawapg.app.trial.composeplayground.ui.theme.ComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,5 +33,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Sample() {
-    // TODO
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(modifier = Modifier
+            .size(50.dp)
+            .background(Color.Blue)
+            .align(Alignment.TopStart)
+            .clickable { println("*** Click ***") }
+        )
+        Box(modifier = Modifier
+            .size(50.dp)
+            .background(Color.Blue)
+            .align(Alignment.Center)
+            .clickable { println("*** Click ***") }
+        )
+        Box(modifier = Modifier
+            .size(50.dp)
+            .background(Color.Blue)
+            .align(Alignment.BottomEnd)
+            .clickable { println("*** Click ***") }
+        )
+    }
 }
