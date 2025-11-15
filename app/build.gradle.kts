487caa250c50af0339c66960bf4ee9fa546bc8b0
usuiat <stripewani@gmail.com>
2025-11-16 07:57:00 +0900

    SDKバージョンを36に更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "net.engawapg.app.trial.composeplayground"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "net.engawapg.app.trial.composeplayground"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
