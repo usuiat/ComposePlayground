278b3e7d4856555b4464c83eb54ef1deeab7db60
usuiat <stripewani@gmail.com>
2025-11-30 00:08:02 +0900

    M3 Expressiveでクッキー形ボタンとFAB menuを作ってみた

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,14 +40,15 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.compose.material3:material3:1.5.0-alpha09")
+    implementation("androidx.compose.material:material-icons-core:1.7.8")
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/net/engawapg/app/trial/composeplayground/MainActivity.kt
+++ app/src/main/java/net/engawapg/app/trial/composeplayground/MainActivity.kt
@@ -1,15 +1,81 @@
 package net.engawapg.app.trial.composeplayground
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonShapes
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.FloatingActionButtonMenu
+import androidx.compose.material3.FloatingActionButtonMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.IconButtonShapes
+import androidx.compose.material3.MaterialShapes.Companion.Cookie7Sided
+import androidx.compose.material3.MaterialShapes.Companion.Ghostish
+import androidx.compose.material3.PlainTooltip
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.ToggleFloatingActionButton
+import androidx.compose.material3.ToggleFloatingActionButtonDefaults.animateIcon
+import androidx.compose.material3.TooltipAnchorPosition
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.animateFloatingActionButton
+import androidx.compose.material3.rememberTooltipState
+import androidx.compose.material3.toShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.isShiftPressed
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.input.key.type
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.CustomAccessibilityAction
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.customActions
+import androidx.compose.ui.semantics.isTraversalGroup
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.stateDescription
+import androidx.compose.ui.semantics.traversalIndex
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.engawapg.app.trial.composeplayground.ui.theme.ComposePlaygroundTheme
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Archive
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Contacts
+import androidx.compose.material.icons.filled.People
+import androidx.compose.material.icons.filled.Snooze
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +83,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposePlaygroundTheme {
-                Box(modifier = Modifier.fillMaxSize().safeDrawingPadding()) {
+                Box(modifier = Modifier.fillMaxSize().safeDrawingPadding(), contentAlignment = Alignment.Center) {
                     Sample()
                 }
             }
@@ -25,7 +91,155 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Preview(showBackground = true)
 @Composable
 fun Sample() {
-    // TODO
+    FilledIconButton(
+        onClick = {},
+        modifier = Modifier.size(IconButtonDefaults.extraLargeContainerSize()),
+        shapes = IconButtonShapes(
+            shape = Cookie7Sided.toShape(),
+            pressedShape = Ghostish.toShape()
+        )
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.android),
+            contentDescription = null,
+            modifier = Modifier.size(IconButtonDefaults.extraLargeIconSize)
+        )
+    }
+}
+
+@SuppressLint("RememberInComposition")
+@OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun FabMenu() {
+    val listState = rememberLazyListState()
+    val fabVisible by remember {
+        derivedStateOf {
+            listState.firstVisibleItemIndex == 0 || listState.canScrollForward == false
+        }
+    }
+    val focusRequester = FocusRequester()
+
+    Box {
+        LazyColumn(state = listState, modifier = Modifier.fillMaxSize()) {
+            for (index in 0 until 100) {
+                item {
+                    Text(
+                        text = "List item - $index",
+                        modifier = Modifier.clickable {}.fillMaxWidth().padding(24.dp),
+                    )
+                }
+            }
+        }
+
+        val items =
+            listOf(
+                Icons.Filled.People to "Reply all",
+                Icons.Filled.Contacts to "Forward",
+                Icons.Filled.Snooze to "Snooze",
+                Icons.Filled.Archive to "Archive",
+            )
+
+        var fabMenuExpanded by rememberSaveable { mutableStateOf(false) }
+
+        BackHandler(fabMenuExpanded) { fabMenuExpanded = false }
+
+        FloatingActionButtonMenu(
+            modifier = Modifier.align(Alignment.BottomEnd),
+            expanded = fabMenuExpanded,
+            button = {
+                // A FAB should have a tooltip associated with it.
+                TooltipBox(
+                    positionProvider =
+                        TooltipDefaults.rememberTooltipPositionProvider(
+                            if (fabMenuExpanded) {
+                                TooltipAnchorPosition.Start
+                            } else {
+                                TooltipAnchorPosition.Above
+                            }
+                        ),
+                    tooltip = { PlainTooltip { Text("Toggle menu") } },
+                    state = rememberTooltipState(),
+                ) {
+                    ToggleFloatingActionButton(
+                        modifier =
+                            Modifier.semantics {
+                                traversalIndex = -1f
+                                stateDescription =
+                                    if (fabMenuExpanded) "Expanded" else "Collapsed"
+                                contentDescription = "Toggle menu"
+                            }
+                                .animateFloatingActionButton(
+                                    visible = fabVisible || fabMenuExpanded,
+                                    alignment = Alignment.BottomEnd,
+                                )
+                                .focusRequester(focusRequester),
+                        checked = fabMenuExpanded,
+                        onCheckedChange = { fabMenuExpanded = !fabMenuExpanded },
+                    ) {
+                        val imageVector by remember {
+                            derivedStateOf {
+                                if (checkedProgress > 0.5f) Icons.Filled.Close else Icons.Filled.Add
+                            }
+                        }
+                        Icon(
+                            painter = rememberVectorPainter(imageVector),
+                            contentDescription = null,
+                            modifier = Modifier.animateIcon({ checkedProgress }),
+                        )
+                    }
+                }
+            },
+        ) {
+            items.forEachIndexed { i, item ->
+                FloatingActionButtonMenuItem(
+                    modifier =
+                        Modifier.semantics {
+                            isTraversalGroup = true
+                            // Add a custom a11y action to allow closing the menu when focusing
+                            // the last menu item, since the close button comes before the first
+                            // menu item in the traversal order.
+                            if (i == items.size - 1) {
+                                customActions =
+                                    listOf(
+                                        CustomAccessibilityAction(
+                                            label = "Close menu",
+                                            action = {
+                                                fabMenuExpanded = false
+                                                true
+                                            },
+                                        )
+                                    )
+                            }
+                        }
+                            .then(
+                                if (i == 0) {
+                                    Modifier.onKeyEvent {
+                                        // Navigating back from the first item should go back to the
+                                        // FAB menu button.
+                                        if (
+                                            it.type == KeyEventType.KeyDown &&
+                                            (it.key == Key.DirectionUp ||
+                                                    (it.isShiftPressed && it.key == Key.Tab))
+                                        ) {
+                                            focusRequester.requestFocus()
+                                            return@onKeyEvent true
+                                        }
+                                        return@onKeyEvent false
+                                    }
+                                } else {
+                                    Modifier
+                                }
+                            ),
+                    onClick = { fabMenuExpanded = false },
+                    icon = { Icon(item.first, contentDescription = null) },
+                    text = { Text(text = item.second) },
+                )
+            }
+        }
+    }
 }
